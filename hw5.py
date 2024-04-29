3990e680128747b1a5c8e8dcf8216c1d4fdc6094
mccartnp <nicholasmccarty252@gmail.com>
2024-04-29 11:38:19 -0400

    implemented data processing

--- hw5.py
+++ hw5.py
@@ -44,3 +44,22 @@ class CommonCity(IZipProcessor):
             states = file.readlines()
         for state in states:
             self.state_cities[state.strip()] = set()
+
+
+  def do_process(self):
+        self.find_common_cities()
+
+    def find_common_cities(self):
+        with open("zipcodes.txt", "r") as file:
+            lines = file.readlines()
+        for line in lines:
+            parts = line.split('\t')
+            if len(parts) > 4:
+                city = parts[3]
+                state = parts[4]
+                if state in self.state_cities:
+                    self.state_cities[state].add(city)
+        common_cities = set.intersection(*self.state_cities.values())
+        with open(self.filename, "w") as file:
+            for city in sorted(common_cities):
+                file.write(f"{city}\n")
